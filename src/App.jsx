f2d2eabb1871037c1f11b06cc82e35529ca448df
Hojiakbar <hojiakbarsobirov30@gmail.com>
2025-01-10 18:38:24 +0300

    saytda axiosinstance qo'shildi

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,28 @@ import PortfolioPage from "./PortfolioPage";
 import ContactPage from "./ContactPage";
 import HomePage from "./HomePage";
 import NavbarPage from "./NavbarPage";
+import { useEffect, useState } from "react";
+import AxiosInstance from './AxiosInstance'
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetchData()
+  },[])
+
+  const fetchData = async() => {
+    try {
+      const response = await AxiosInstance.get('portfolio')
+      setData(response.data)
+    } catch (err) {
+      console.log('Error !')
+    }
+  }
+
+  console.log(data)
+
   return (
     <>
     <NavbarPage/>
--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
+import AxiosInstance from "./AxiosInstance";
 
 const ContactPage = () => {
+
+  const nameRef = useRef('')
+  const emailRef = useRef('')
+  const phoneRef = useRef('')
+  const messageRef = useRef('')
+
+  const createData = async(e) => {
+    e.preventDefault()
+
+
+    const newData = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      phone: phoneRef.current.value,
+      message: messageRef.current.value
+    }
+
+    try {
+      const response = await AxiosInstance.post('portfolio', newData)
+    } catch (err) {
+      alert('Error !')
+    }
+  }
+
   return (
     
-    <section className="bg-gray-200 w-full h-auto p-5">
+    <section className="bg-gray-200 w-full h-auto p-10">
       <div className=" w-full h-auto p-5 flex justify-center items-center">
         <div className="font-bold text-4xl border-[6px] border-black w-[230px] flex justify-center items-center px-4 py-3">
           CONTACT
@@ -19,26 +44,30 @@ const ContactPage = () => {
       </div>
 
       <header className=" w-full h-[75vh] flex justify-center items-center">
-        <form className=" w-[380px] h-[500px] flex justify-around items-center flex-col">
+        <form onSubmit={createData} className=" w-[380px] h-[500px] flex justify-around items-center flex-col">
           <input
+          ref={nameRef}
             className="w-[90%] h-12 text-sm border-b-4 border-l-4 pl-4 border-black"
             type="text"
             placeholder="ENTER YOUR NAME*"
           />
 
           <input
+          ref={emailRef}
             className="w-[90%] text-sm h-12 border-b-4 border-l-4 pl-4 border-black"
             type="text"
             placeholder="ENTER YOUR EMAIL*"
           />
 
           <input
+          ref={phoneRef}
             className="w-[90%] text-sm h-12 border-b-4 border-l-4 pl-4 border-black"
             type="number"
             placeholder="PHONE NUMBER"
           />
 
           <textarea
+          ref={messageRef}
             className="w-[90%] h-28 border-b-4 border-l-4 border-black pl-4 text-sm py-2"
             placeholder="YOUR MESSAGE*"
             name=""
@@ -46,7 +75,7 @@ const ContactPage = () => {
           ></textarea>
 
           <div className="w-full flex justify-center items-center">
-            <button className="font-medium border-r-[3px] border-l-[3px] border-black px-4">
+            <button type="submit" className="font-medium border-r-[3px] border-l-[3px] border-black px-4">
               SUBMIT
             </button>
           </div>
