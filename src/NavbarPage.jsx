020e572650dfd867a1d7ad7674b02018dde24345
Hojiakbar <hojiakbarsobirov30@gmail.com>
2025-01-12 08:04:05 +0300

    Navbar o'zgartirildi

--- src/NavbarPage.jsx
+++ src/NavbarPage.jsx
@@ -9,6 +9,10 @@ const NavbarPage = () => {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <section className="bg-white w-full h-20 flex justify-between items-center px-5 md:px-20 sticky top-0 shadow-lg">
       <img className="w-10" src={NavLogo} alt="Logo" />
@@ -27,27 +31,41 @@ const NavbarPage = () => {
         } absolute top-16 left-0 bg-white w-full shadow-lg md:static md:flex md:space-x-10 md:shadow-none font-medium items-center text-[18px]`}
       >
         <li className="border-b md:border-none">
-          <Link className="block py-2 px-5 md:py-0" to={"/"}>
+          <Link className="block py-2 px-5 md:py-0" to={"/"} onClick={closeMenu}>
             Home
           </Link>
         </li>
         <li className="border-b md:border-none">
-          <Link className="block py-2 px-5 md:py-0" to={"/about"}>
+          <Link
+            className="block py-2 px-5 md:py-0"
+            to={"/about"}
+            onClick={closeMenu}
+          >
             About me
           </Link>
         </li>
         <li className="border-b md:border-none">
-          <Link className="block py-2 px-5 md:py-0" to={"/skills"}>
+          <Link
+            className="block py-2 px-5 md:py-0"
+            to={"/skills"}
+            onClick={closeMenu}
+          >
             Skills
           </Link>
         </li>
         <li className="border-b md:border-none">
-          <Link className="block py-2 px-5 md:py-0" to={"/portfolio"}>
+          <Link
+            className="block py-2 px-5 md:py-0"
+            to={"/portfolio"}
+            onClick={closeMenu}
+          >
             Portfolio
           </Link>
         </li>
         <button className="block bg-black text-white text-sm px-3 py-2 rounded-full mx-auto md:mx-0">
-          <Link to={"/contact"}>Contact me</Link>
+          <Link to={"/contact"} onClick={closeMenu}>
+            Contact me
+          </Link>
         </button>
       </ul>
     </section>
